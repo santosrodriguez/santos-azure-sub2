37279543fc584722399458e1daf1a0eda1bde8f5
Santos Rodriguez <santos.rodriguez@hotmail.com>
2023-07-03 12:59:18 +0000

    Updated main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,7 +5,7 @@ provider "azurerm" {
   client_id       = "da407e9f-5e21-47d4-808e-21382cabf2b5"
   client_secret   = "zaK8Q~ZdU6RMj97dlU3pMWwPIehJU~rMYlfY-co5"
   tenant_id       = "603b3704-d0cf-4517-9067-6afd49a51d1b"
-  subscription_id = "2aa7f81a-c2cc-41f6-ae8e-468be407d0de"
+  subscription_id = "d4693006-ef9a-4c42-a3ec-a6e69a58f5ee"
 }
 terraform {
   backend "remote" {
