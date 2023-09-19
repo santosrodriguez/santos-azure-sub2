d4313995d02b88d5317d7210509c10e5fdb2d245
Santos Rodriguez <santos.rodriguez@caresource.com>
2023-09-19 14:41:22 -0400

    Moved variables to variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,6 +1,6 @@
-#################################################
-# MAIN VARIABLES                                #
-#################################################
+##########################
+## General VARIABLES      #
+##########################
 
 # Change this variable to match the target subscription.
 variable "sub_Name" {
@@ -8,8 +8,6 @@ variable "sub_Name" {
   description = "Name of the target subscription"
   default     = "santos-azure-sub2"
 }
-
-
 
 
 # Region where the resource will be deployed
@@ -29,7 +27,9 @@ variable "dns_entries_eastus" {
 
 
 
-
+##########################
+## Providers VARIABLES  ##
+##########################
 
 ### The variables below are set in Terraform Cloud. Empty variables are required to be set here for the Terraform cloud variables to work
 
@@ -39,7 +39,6 @@ variable "subscription_id" {
   description = "Id of the LOCAL (target) subscription"
 }
 
-
 #Azure AD tenant ID
 variable "tenant_id" {
   type        = string
@@ -48,3 +47,100 @@ variable "tenant_id" {
 variable "client_id" {}
 variable "client_secret" {}
 
+
+
+
+######################
+## Virtual Networks ##
+######################
+
+#Varibles related to Vnet 2 : spoke
+
+variable "vnet2_resource_group_name" {
+type = string
+description = "name of the ressource group"
+}
+variable "vnet2_name" {
+type = string
+description = "Names of the spoke virtual network"
+}
+variable "vnet2_id" {
+description = "Id of the spoke virtual network"
+}
+variable "allow_virtual_network_access_vnet2_to_vnet1" {
+type = bool
+description = "(Optional) Controls if the VMs in the remote virtual network can access VMs in the local virtual network. default to true."
+default = true
+}
+variable "allow_forwarded_traffic_vnet2_to_vnet1" {
+type = bool
+description = "(Optional) Controls if forwarded traffic from VMs in the remote virtual network is allowed. default to false."
+default = true
+}
+variable "allow_gateway_transit_vnet2_to_vnet1" {
+type = bool
+description = "(Optional) Controls gatewayLinks can be used in the remote virtual network’s link to the local virtual network."
+default = false
+}
+variable "use_remote_gateways_vnet2_to_vnet1" {
+type = bool
+description = "(Optional) Controls if remote gateways can be used on the local virtual network. If the flag is set to true, and allow_gateway_transit on the remote peering is also true, virtual network will use gateways of remote virtual network for transit. Only one peering can have this flag set to true. This flag cannot be set if virtual network already has a gateway. default to false."
+default = false
+}
+
+#variables related to Vnet 1 : hub
+variable "vnet1_resource_group_name" {
+type = string
+description = "name of the ressource group"
+}
+variable "vnet1_name" {
+type = string
+description = "Names of the hub virtual network"
+}
+variable "vnet1_id" {
+description = "Id of the spoke virtual network"
+}
+variable "allow_virtual_network_access_vnet1_to_vnet2" {
+type = bool
+description = "(Optional) Controls if the VMs in the remote virtual network can access VMs in the local virtual network. default to true."
+default = true
+}
+variable "allow_forwarded_traffic_vnet1_to_vnet2" {
+type = bool
+description = "(Optional) Controls if forwarded traffic from VMs in the remote virtual network is allowed. default to false."
+default = true
+}
+variable "allow_gateway_transit_vnet1_to_vnet2" {
+type = bool
+description = "(Optional) Controls gatewayLinks can be used in the remote virtual network’s link to the local virtual network."
+default = true
+}
+variable "use_remote_gateways_vnet1_to_vnet2" {
+type = bool
+description = "(Optional) Controls if remote gateways can be used on the local virtual network. If the flag is set to true, and allow_gateway_transit on the remote peering is also true, virtual network will use gateways of remote virtual network for transit. Only one peering can have this flag set to true. This flag cannot be set if virtual network already has a gateway. default to false."
+default = false
+}
+variable "vnet2_client_id" {
+description = "vnet2 SP creds for provider"
+}
+variable "vnet2_tenant_id" {
+description = "vnet2 SP creds for provider"
+}
+variable "vnet2_client_secret" {
+description = "vnet2 SP creds for provider"
+}
+variable "vnet2_subscription_id" {
+description = "vnet2 SP creds for provider"
+}
+variable "vnet1_client_id" {
+description = "vnet1 SP creds for provider"
+}
+variable "vnet1_tenant_id" {
+description = "vnet1 SP creds for provider"
+}
+variable "vnet1_client_secret" {
+description = "vnet1 SP creds for provider"
+}
+variable "vnet1_subscription_id" {
+description = "vnet1 SP creds for provider"
+}
